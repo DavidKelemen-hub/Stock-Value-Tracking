8f0a394a9dbadb4d42e38882bcf40ff721539463
DavidKelemen-hub <94431598+DavidKelemen-hub@users.noreply.github.com>
2025-12-19 21:20:45 +0200

    Update Processing.cs

--- StockApp_Classes/StockApp_Classes/Processing/Processing.cs
+++ StockApp_Classes/StockApp_Classes/Processing/Processing.cs
@@ -15,7 +15,54 @@ namespace StockApp_Classes.Processing
         {
             return data.Max(x => Convert.ToDouble(x.Value.High));
         }
-        
+
+        public double GetLowestPrice(Dictionary<string, DailyEntry> data)
+        {
+            return data.Min(x => Convert.ToDouble(x.Value.Low));
+        }
+
+        public double GetHighestClosingPrice(Dictionary<string, DailyEntry> data)
+        {
+            return data.Max(x => Convert.ToDouble(x.Value.Close));
+        }
+
+        public double GetLowestClosingPrice(Dictionary<string, DailyEntry> data)
+        {
+            return data.Min(x => Convert.ToDouble(x.Value.Close));
+        }
+
+        public double GetHighestOpeningPrice(Dictionary<string, DailyEntry> data)
+        {
+            return data.Max(x => Convert.ToDouble(x.Value.Open));
+        }
+
+        public double GetLowestOpeningPrice(Dictionary<string, DailyEntry> data)
+        {
+            return data.Min(x => Convert.ToDouble(x.Value.Open));
+        }
+
+        public double GetHighestVolume(Dictionary<string, DailyEntry> data)
+        {
+            return data.Max(x => Convert.ToDouble(x.Value.Volume));
+        }
+
+        public double GetLowestVolume(Dictionary<string, DailyEntry> data)
+        {
+            return data.Min(x => Convert.ToDouble(x.Value.Volume));
+        }
+
+        public string GetHighestPriceDate(Dictionary<string, DailyEntry> data)
+        {
+            var highest = data.OrderByDescending(x => Convert.ToDouble(x.Value.High)).First();
+            return highest.Key;
+        }
+
+        public string GetLowestPriceDate(Dictionary<string, DailyEntry> data)
+        {
+            var lowest = data.OrderBy(x => Convert.ToDouble(x.Value.Low)).First();
+            return lowest.Key;
+        }
+
     }
 }
 
