df7c6ee8f8c54912bc15109dd12b601c707177fc
DavidKelemen-hub <94431598+DavidKelemen-hub@users.noreply.github.com>
2025-12-20 18:52:03 +0200

    more getter functions

--- StockApp_Classes/StockApp_Classes/Processing/Processing.cs
+++ StockApp_Classes/StockApp_Classes/Processing/Processing.cs
@@ -10,7 +10,7 @@ namespace StockApp_Classes.Processing
 
     public class Processing
     {
-        
+
         public double GetHighestPrice(Dictionary<string, DailyEntry> data)
         {
             return data.Max(x => Convert.ToDouble(x.Value.High));
@@ -63,6 +63,82 @@ namespace StockApp_Classes.Processing
             return lowest.Key;
         }
 
+        public double GetOpenPriceByDate(Dictionary<string, DailyEntry> data, string date)
+        {
+            if (data.ContainsKey(date))
+            {
+                return Math.Round(Convert.ToDouble(data[date].Open), 2);
+            }
+            throw new ArgumentException("Date not found in data.");
+        }
+
+        public double GetClosePriceByDate(Dictionary<string, DailyEntry> data, string date)
+        {
+            if (data.ContainsKey(date))
+            {
+                return Math.Round(Convert.ToDouble(data[date].Close), 2);
+            }
+            throw new ArgumentException("Date not found in data.");
+        }
+
+        public double GetHighPriceByDate(Dictionary<string, DailyEntry> data, string date)
+        {
+            if (data.ContainsKey(date))
+            {
+                return Math.Round(Convert.ToDouble(data[date].High), 2);
+            }
+            throw new ArgumentException("Date not found in data.");
+        }
+
+        public double GetLowPriceByDate(Dictionary<string, DailyEntry> data, string date)
+        {
+            if (data.ContainsKey(date))
+            {
+                return Math.Round(Convert.ToDouble(data[date].Low), 2);
+            }
+            throw new ArgumentException("Date not found in data.");
+        }
+
+        public int GetVolumeByDate(Dictionary<string, DailyEntry> data, string date)
+        {
+            if (data.ContainsKey(date))
+            {
+                return Convert.ToInt32(data[date].Volume);
+            }
+            throw new ArgumentException("Date not found in data.");
+        }
+
+        public double GetAverageClosingPrice(Dictionary<string, DailyEntry> data)
+        {
+            return Math.Round(data.Average(x => Convert.ToDouble(x.Value.Close)), 2);
+        }
+
+        public double GetAverageOpeningPrice(Dictionary<string, DailyEntry> data)
+        {
+            return Math.Round(data.Average(x => Convert.ToDouble(x.Value.Open)), 2);
+        }
+
+        public double GetAverageHighPrice(Dictionary<string, DailyEntry> data)
+        {
+            return Math.Round(data.Average(x => Convert.ToDouble(x.Value.High)), 2);
+        }
+
+        public double GetAverageLowPrice(Dictionary<string, DailyEntry> data)
+        {
+            return Math.Round(data.Average(x => Convert.ToDouble(x.Value.Low)), 2);
+
+        }
+
+        public double GetPercentChange(Dictionary<string, DailyEntry> data, string startDate, string endDate)
+        {
+            if (data.ContainsKey(startDate) && data.ContainsKey(endDate))
+            {
+                double startPrice = Convert.ToDouble(data[startDate].Close);
+                double endPrice = Convert.ToDouble(data[endDate].Close);
+                return Math.Round(((endPrice - startPrice) / startPrice) * 100, 2);
+            }
+            throw new ArgumentException("One or both dates not found in data.");
+        }
     }
 }
 
