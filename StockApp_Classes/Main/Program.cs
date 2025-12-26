f4dda903aa6a72e3ffaa9ba034d8d8930d51066e
DavidKelemen-hub <94431598+DavidKelemen-hub@users.noreply.github.com>
2025-12-26 21:55:46 +0200

    refactoring

--- StockApp_Classes/Main/Program.cs
+++ StockApp_Classes/Main/Program.cs
@@ -1,28 +1,23 @@
 ﻿using StockApp_Classes.Services;
 using StockApp_Classes.Processing;
+using System.Runtime.Serialization;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        string filePath = "C:\\Repos\\Stock-Value-Tracking\\StockApp_Classes\\StockApp_Classes\\Resources\\sp100.json";
-        StockService stockService = new StockService();
-        Processing processing = new Processing();
-        SymbolService symbolService = new SymbolService(filePath);
+       const string connectionstring = "Data Source=localhost;Initial Catalog=StockData;Integrated Security=True;Trust Server Certificate=True";
 
-        var symbols = symbolService.GetAllSymbols();
+        DataBaseService service = new DataBaseService(connectionstring);
+
+       var result = service.GetCompanyIDFromName("AbbVie");
+
+       //foreach (var entry in result)
+       //{
+       // Console.WriteLine($"{entry.TradeDate} - Open: {entry.OpenPrice}, Close: {entry.ClosePrice}");
+       // }
+        Console.WriteLine(result);
 
 
-        foreach(var item in symbols)
-        {
-            
-            var data = await stockService.GetTimeSeriesDataAsync(item.Symbol);
-            var highestPrice = processing.GetHighestClosingPrice(data);
-            var highestPriceDate = processing.GetHighestClosingPriceDate(data);
-            Console.WriteLine($"{item.Name} - {item.Symbol} Highest Price: {highestPrice} on {highestPriceDate}");
-            Thread.Sleep(12000); // To respect API rate limits
-        }
-
-        
     }
 }
---
+++ StockApp_Classes/StockApp_Classes/Services/DataBaseService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using StockApp_Classes.Models;
+
+namespace StockApp_Classes.Services
+{
+    public class DataBaseService
+    {
+        private readonly string connectionString;
+        public DataBaseService(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        const string queryString = "SELECT Name FROM Company";
+
+        public List<string> GetAllCompanies()
+        {
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                var result = connection.Query<string>(queryString).ToList();
+                return result;
+            }
+        }
+
+        public int GetCompanyIDFromSymbol(string symbol)
+        {
+            const string queryString = "SELECT StockID FROM Company WHERE Symbol = @symbol";
+
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                var result = connection.QuerySingleOrDefault<int>(queryString, new { symbol });
+                return result;
+            }
+        }
+
+        public int GetCompanyIDFromName(string name)
+        {
+            const string queryString = "SELECT StockID FROM Company WHERE Name = @name";
+
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                var result = connection.QuerySingleOrDefault<int>(queryString, new { name });
+                return result;
+            }
+        }
+
+        public List<DailyEntry> GetCompleteStockData(string symbol)
+        {
+            var stockID = GetCompanyIDFromSymbol(symbol);
+            const string queryString = "SELECT TradeDate, OpenPrice, HighPrice, LowPrice, ClosePrice, Volume " +
+                                       "FROM DailyPrices WHERE StockID = @stockID";
+
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                var result = connection.Query<DailyEntry>(queryString, new { stockID });
+                return result.ToList();
+            }
+        }
+
+        public List<DailyEntry> GetStockEntriesBetweenDates(string symbol, string startDate, string endDate)
+        {
+            var stockID = GetCompanyIDFromSymbol(symbol);
+            const string queryString = "SELECT TradeDate, OpenPrice, HighPrice, LowPrice, ClosePrice, Volume " +
+                                       "FROM DailyPrices WHERE StockID = @stockID " +
+                                       "AND TradeDate BETWEEN @startDate AND @endDate";
+
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                var result = connection.Query<DailyEntry>(queryString, new { stockID, startDate, endDate });
+                return result.ToList();
+            }
+        }
+
+
+    }
+}
